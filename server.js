a3c28f0f4130529f33f1296f67ffd454e5c659ea
Wakeel Kehinde Ige <tijjken@gmail.com>
2021-10-04 01:33:27 +0100

    added port

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
-app.listen(3000, () => {
-  console.log("app is running");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.conv}`);
 });
